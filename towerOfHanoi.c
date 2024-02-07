0db5a2811096a2e1ffc946fc4651575dd570b428
TShreek <124446964+TShreek@users.noreply.github.com>
2024-02-07 22:53:05 +0530

    Update towerOfHanoi.c

--- towerOfHanoi.c
+++ towerOfHanoi.c
@@ -2,56 +2,24 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct emp
-{
-    char name[20];
-    int emp_id;
-    char dept[20];
-    float sal;
-};
+static int step=0;
 
-void printDetails(struct emp* ptr, int n)
+void toh(int n, char source, char spare, char dest)
 {
-    for(int i = 0; i < n; i++)
-    {
-        printf("\n The employee details of employee %d are :", i + 1);
-        printf("\n Name : %s\n Emp ID : %d \n Dept name : %s \n Salary: %f", ptr[i].name, ptr[i].emp_id, ptr[i].dept, ptr[i].sal);
-    }
+	if(n==1)
+		printf("step %d : move %d from %c to %c\n",++step,n,source,dest);
+	else
+	{
+     toh(n-1,source,dest,spare);
+	 toh(1,source,spare,dest);
+	 toh(n-1,spare,source,dest);
+	}
 }
-
-void deptSal(struct emp* ptr, int n)
+void main()
 {
-    char dept_name[20];
-    printf("\nEnter the name of the department: ");
-    scanf("%s", dept_name);
-    float sum = 0;
-    for(int i = 0; i < n; i++)
-    {
-        if(strcmp(dept_name, (ptr + i)->dept) == 0)
-            sum += (ptr + i)->sal;
-    }
-    printf("\n The total salary of the department is %f", sum);
-}
-
-int main()
-{
-    printf("\n Enter the number of employees: ");
-    int n;
-    scanf("%d", &n);
-    struct emp *p = (struct emp*)malloc(n * sizeof(struct emp));
-    for(int i = 0; i < n; i++)
-    {
-        printf("\n Enter employee name: ");
-        scanf("%s", p[i].name);
-        printf("\n Enter employee ID: ");
-        scanf("%d", &p[i].emp_id);
-        printf("\n Enter the department of the employee: ");
-        scanf("%s", p[i].dept);
-        printf("\n Enter the employee salary: ");
-        scanf("%f", &p[i].sal);
-    }
-    printDetails(p, n);
-    deptSal(p, n);
-    free(p);
-    return 0;
+printf("Enter the number of rings: ");
+int n;
+scanf("%d",&n);
+char a='A',b='B',c='C';
+toh(n,a,b,c);
 }
